be4a1ff5a0746421234347d042c7557d24c796c1
Yannik Mewes <103323606+yakinikun1337@users.noreply.github.com>
2024-01-18 11:50:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/add", async (req, res) => {
 try {
   const result = await db.query(
     "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
-    [input]
+    [input.toLowerCase()]
   );
 
     const data = result.rows[0];
